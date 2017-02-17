b6fe989bb5eba874091961a623315c1cd63f78cd
Pierre Chamberlain <bigp@pierrechamberlain.ca>
2017-02-17 18:15:25 -0400

    Adding redis support.

--- nodelib/sv-database.js
+++ nodelib/sv-database.js
@@ -6,9 +6,40 @@ const Schema = mongoose.Schema;
 const Model = mongoose.Model;
 const Query = mongoose.Query;
 const ObjectID = Schema.ObjectId;
+const Redis = require('redis');
+const RedisServer = require('redis-server');
 
 module.exports = function(ERDS) {
-	trace("Mongo Disabled for now.".yellow);
+	var server = new RedisServer();
+	trace("server....");
+	trace(server);
+	
+	server.open(err => {
+		if(err) return traceError("Redis Server could not be started");
+		
+		trace("Redis Server started: " + server.port);
+		startRedisClient();
+	}).then(() => {
+		trace("?????? started?");
+	});
+
+	server.on('connect', (err)=> {
+		trace("????????");
+	});
+	
+	function startRedisClient() {
+		var client = ERDS.redis = Redis.createClient();
+		client.on('error', function(err) {
+			traceError("Redis failed to start: " + err);
+		});
+
+		client.on('connect', function() {
+			trace("Connected Redis! :)");
+		});
+	}
+	
+	trace("(Mongo Disabled for now)".yellow);
+	
 	return;
 
 	mongoose
